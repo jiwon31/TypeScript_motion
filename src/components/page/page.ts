5096fc5f63e59969b83bdab4fb2ac21fdde4fd33
jiwon Nam <swm10099@naver.com>
2022-10-17 00:48:22 +0900

    refactor: DI 리팩토링

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -5,16 +5,26 @@ export interface Composable {
 }
 type OnCloseListener = () => void;
 
-class PageItemComponent
+interface SectionContainer extends Component, Composable {
+  setOnCloseListener(listener: OnCloseListener): void;
+}
+
+type SectionContainerConstructor = {
+  new (): SectionContainer;
+};
+
+export class PageItemComponent
   extends BaseComponent<HTMLElement>
-  implements Composable
+  implements SectionContainer
 {
   private closeListener?: OnCloseListener;
 
   constructor() {
     super(`<li class="page-item">
               <section class="page-item__body"></section>
-              <div class="page-item__controls"><button class="close">&times;</button></div>
+              <div class="page-item__controls">
+                <button class="close">&times;</button>
+              </div>
             </li>`);
 
     const closeBtn = this.element.querySelector(".close")! as HTMLButtonElement;
@@ -39,12 +49,12 @@ export class PageComponent
   extends BaseComponent<HTMLUListElement>
   implements Composable
 {
-  constructor() {
+  constructor(private pageItemConstructor: SectionContainerConstructor) {
     super('<ul class="page"></ul>');
   }
 
   addChild(section: Component) {
-    const item = new PageItemComponent();
+    const item = new this.pageItemConstructor();
     item.addChild(section);
     item.attachTo(this.element, "beforeend");
     item.setOnCloseListener(() => {
