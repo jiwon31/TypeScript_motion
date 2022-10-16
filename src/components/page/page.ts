28301e78db291aadccd9d754c8efa975d5036e50
jiwon Nam <swm10099@naver.com>
2022-10-16 23:47:27 +0900

    feat: Page item 삭제 기능 구현

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -3,16 +3,24 @@ import { BaseComponent, Component } from "../component.js";
 export interface Composable {
   addChild(child: Component): void;
 }
+type OnCloseListener = () => void;
 
 class PageItemComponent
   extends BaseComponent<HTMLElement>
   implements Composable
 {
+  private closeListener?: OnCloseListener;
+
   constructor() {
     super(`<li class="page-item">
               <section class="page-item__body"></section>
               <div class="page-item__controls"><button class="close">&times;</button></div>
             </li>`);
+
+    const closeBtn = this.element.querySelector(".close")! as HTMLButtonElement;
+    closeBtn.onclick = () => {
+      this.closeListener && this.closeListener();
+    };
   }
 
   addChild(child: Component) {
@@ -20,6 +28,10 @@ class PageItemComponent
       ".page-item__body"
     )! as HTMLElement;
     child.attachTo(container);
+  }
+
+  setOnCloseListener(listener: OnCloseListener) {
+    this.closeListener = listener;
   }
 }
 
@@ -35,5 +47,8 @@ export class PageComponent
     const item = new PageItemComponent();
     item.addChild(section);
     item.attachTo(this.element, "beforeend");
+    item.setOnCloseListener(() => {
+      item.removeFrom(this.element);
+    });
   }
 }
