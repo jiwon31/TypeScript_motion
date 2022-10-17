64a0fb96e688975a2b7a336cb95feb75a0fe7435
jiwon Nam <swm10099@naver.com>
2022-10-17 23:29:43 +0900

    refactor: todo checkbox label 추가

--- src/components/page/item/image.ts
+++ src/components/page/item/image.ts
@@ -4,7 +4,7 @@ export class ImageComponent extends BaseComponent<HTMLElement> {
   constructor(title: string, url: string) {
     super(`<section class="image">
               <div class="image__holder"><img class="image__thumnail" /></div>
-              <h2 class="image__title"></h2>
+              <h2 class="page-item__title image__title"></h2>
             </section>`);
 
     // 사용자에게 전달받은 인자를 바로 innerHTML로 설정하는 것은 위험하다.
